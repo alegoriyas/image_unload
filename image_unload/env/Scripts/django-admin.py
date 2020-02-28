c87a0ee2027cb453e35e2fc9e105633fbfe3952e
Стеблина Елена Евгеньевна <@kubstu.edu>
2020-02-28 08:49:51 +0300

    Add project files.

---
+++ image_unload/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\СтеблинаЕЕ\source\repos\Django\image_unload\image_unload\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
